f5e892d6a6f0b0f85edba33626b174a2d2490744
Andrew <squaids449@gmail.com>
2020-01-09 16:39:37 -0700

    impliments scroll position shift with nav

--- src/script.js
+++ src/script.js
@@ -1,2 +1,19 @@
-const nav = document.querySelector('#main-nav')
+const navItem = document.querySelectorAll('.nav-item')
 
+const section = {
+    "about": document.querySelector('#about'),
+    "projects": document.querySelector('#projects'),
+    "contact": document.querySelector('#contact')
+}
+
+window.addEventListener("scroll", event => {
+    console.log(window.scrollY)
+})
+
+navItem.forEach(tab => {
+    tab.addEventListener('click', event => {
+        const sectName = event.target.textContent.toLowerCase()
+        section[sectName].scrollIntoView({behavior: "smooth"})
+        console.log(window.scrollY)
+    })
+})
